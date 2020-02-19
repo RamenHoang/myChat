83c28ca4e38e534bb2690206f66d695550c2af1c
RamenHoang <anhnguyenhoang321@gmail.com>
2020-02-19 17:25:14 +0700

    [1.0.0] Add static page

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
 import express from 'express';
-import connectDb from './config/connectDb.js';
-import ConntactModel from './models/contact.model.js';
+import connectDb from './config/connectDb';
+import configViewEngine from './config/viewEngine';
 
+// Init app
 let app = express();
+
+// Connect to mongoDB
 connectDb();
+
+// Config view engine
+configViewEngine(app);
 
 let hostname = process.env.APP_HOSTNAME;
 let port = process.env.APP_PORT;
 
-app.get('/testDB', async (req, res) => {
-	try {
-		let item = {
-			userId: '123123',
-			contactId: '123213'
-		};
-
-		let contact = await ConntactModel.createNew(item);
-		res.send(contact);
-	} catch(err) {
-		console.log(err);
-	}
+app.get('/', (req, res) => {
+	return res.render('main/master');
 });
+
+app.get('/login-register', (req, res) => {
+	return res.render('auth/loginRegister');
+})
 
 app.listen(port, hostname, () => {
 	console.log(`Hello Ramen, I'm running at ${hostname}:${port}/`);
