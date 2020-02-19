ec1cdea827c766ca39e4002f33b9f01e91f06072
RamenHoang <anhnguyenhoang321@gmail.com>
2020-02-19 20:45:51 +0700

    [1.0.0] add routes and controllers

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDb from './config/connectDb';
 import configViewEngine from './config/viewEngine';
+import initRoutes from './routes/web';
 
 // Init app
 let app = express();
@@ -11,16 +12,11 @@ connectDb();
 // Config view engine
 configViewEngine(app);
 
+// Init all routes
+initRoutes(app);
+
 let hostname = process.env.APP_HOSTNAME;
 let port = process.env.APP_PORT;
-
-app.get('/', (req, res) => {
-	return res.render('main/master');
-});
-
-app.get('/login-register', (req, res) => {
-	return res.render('auth/loginRegister');
-})
 
 app.listen(port, hostname, () => {
 	console.log(`Hello Ramen, I'm running at ${hostname}:${port}/`);
